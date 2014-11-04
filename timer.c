77450bf6b4048063236e4afa203cd8b8a9f5176d
anoop chandu <anoopchandu18@gmail.com>
2014-11-05 00:22:12 +0530

    first version-1modi

--- timer.c
+++ timer.c
@@ -3,11 +3,21 @@
 #include<windows.h>
 int main()
 {
-    int i;
+    int i,j=0,k;
     for(i=0;;i++)
     {
-        printf("%d",i);
-        Sleep(1000);
-        printf("\r");
+        printf("%02d",j);
+        goto start;
+        j++;
     }
+    start:
+    for(k=0;k<60;k++)
+     {
+        printf("%02d",k);
+        sleep(1000);
+        printf("\b\b");
+
+      }
 }
+
+
