9cdeaebddfb3b0387587fff2efc5b9d3235b06a4
anoop chandu <anoopchandu.bits@gmail.com>
2014-11-06 10:32:06 +0530

    hour added

--- timer (v2).c
+++ timer (v2).c
@@ -3,22 +3,34 @@
 #include<windows.h>
 int minit(int);
 int sec();
+int hour(int);
 int main()
 {
-    int i,j=0,k;
+    int i,j=0,k=0;
     for(i=0;;i++)
     {
+        hour(k);
         minit(j);
         sec();
         j++;
         printf("\b\b\b");
+        if(j==60)
+        {j=0;
+         k++;
+        }
     }
+}
+
+int hour(int k)
+{
+    printf("%02d:",k);
 }
 
 int minit(int j)
 {
     printf("%02d:",j);
 }
+
 int sec()
 {
     int j;
@@ -29,6 +41,4 @@ int sec()
         printf("\b\b");
     }
 }
-
-
 
