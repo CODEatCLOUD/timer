aa3e9129c4fd0465e32426535821fc34673b6241
anoop chandu <anoopchandu.bits@gmail.com>
2014-11-07 23:58:56 +0530

    futhur connected

--- stopwatch.cpp
+++ stopwatch.cpp
@@ -5,12 +5,18 @@
 
 int sec(int);
 int minit(int);
-
+int minute(int,int);
 int main()
 {
     int m,s;
     printf("mm:ss\n");
     scanf("%d:%d",&m,&s);
+    minute(m,s);
+
+}
+
+int minute(int m,int s)
+{
     for(;m>=0;)
     {
         minit(m);
@@ -20,11 +26,11 @@ int main()
         m--;
     }
 }
-
 int sec(int s)
 {
     for(;s>0;s--)
     {
+
         printf("%02d",s);
         Sleep(1000);
         printf("\b\b");
