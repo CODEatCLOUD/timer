8c912ae2e6e3c4a63871d9d710cf4fb76d3e2b16
anoop chandu <anoopchandu.bits@gmail.com>
2014-11-07 16:47:49 +0530

    xxxx

---
+++ stopwatch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int sec();
+int minit();
+int hour();
+
+int main()
+{
+    int h,m,s;
+    printf("hh:mm:ss");
+    scanf("%d,%d,%d",h,m,s);
+    secr();
+    for(;==0;)
+    {
+        sec();
+        minit();
+        hour();
+    }
+}
+
